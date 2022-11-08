bb1463d438d7b3f38475f3d757b6a7fad1ffe82b
ewaldschrader <117187912+ewaldschrader@users.noreply.github.com>
2022-11-08 17:59:55 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,4 +2,12 @@ def buildApp() {
   echo 'building the application'
 }
 
+def testApp() {
+  echo 'testing the application'
+}
+
+def deployApp() {
+  echo 'deploying the application'
+}
+
 return this
